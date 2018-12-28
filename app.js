ee297d5f1123c74270f495ce3334aa9f02dd35b4
george <geomarray@gmail.com>
2018-12-27 18:52:47 -0800

    working app before testing and documentation

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 'use strict';
 
 const fs = require('fs');
+const util = require('util');
+const event = require('./events');
+const emits = require('./emits');
+const readFile = util.promisify(fs.readFile);
+
 
 const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
+  readFile(file)
+  .then(data => {
+    event.emit('saved');
     let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
+
+    fs.writeFile( file, text, (err, data) => {
       if(err) { throw err; }
       console.log(`${file} saved`);
-    });
-  });
+    })
+  }).catch(err => {
+    event.emit('error', err);
+  })
 };
 
 let file = process.argv.slice(2).shift();
+
 alterFile(file);
